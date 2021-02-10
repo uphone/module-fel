22fe9b28d26b9eece8171b0c3726ed62368ca921
uphone <595228808@qq.com>
2021-02-10 12:33:28 +0800

    optimize

--- src/main/java/com/rino/fel/ExpressUtil.java
+++ src/main/java/com/rino/fel/ExpressUtil.java
@@ -2,7 +2,6 @@ package com.rino.fel;
 
 import com.rino.fel.context.FelContext;
 import com.rino.fel.context.MapContext;
-import com.rino.fel.function.ext.NumberFunction;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
@@ -70,6 +69,24 @@ public final class ExpressUtil {
         return ret.toString();
     }
 
+    // 简单格式化,不需要engine
+    public static String simpleFormat(String el, Map context) {
+        Pattern regex = Pattern.compile("\\$\\{(.*?)}");
+        Matcher matcher = regex.matcher(el);
+        StringBuffer ret = new StringBuffer();
+        while (matcher.find()) {
+            String express = matcher.group();
+            String key = express.substring(0, express.length() - 1).substring(2);
+            Object value = context.get(key);
+            String str = value == null ? "" : value.toString();
+            if ("\\".equals(str)) str = "\\\\";
+            if ("$".equals(str)) str = "\\$";
+            matcher.appendReplacement(ret, str);
+        }
+        matcher.appendTail(ret);
+        return ret.toString();
+    }
+
 
     // 单次调用
     public static Object parse(String express) {
@@ -99,10 +116,9 @@ public final class ExpressUtil {
         FelContext context = engine.getContext();
         if (context == null) context = new MapContext();
         engine.setContext(context);
+        final FelContext ctx = context;
         if (cp instanceof Map) {
-            for (Map.Entry<String, Object> e : ((Map<String, Object>) cp).entrySet()) {
-                context.set(e.getKey(), e.getValue());
-            }
+            ((Map) cp).forEach((k, v) -> ctx.set((String) k, v));
         } else {
             BeanInfo targetBeanInfo = Introspector.getBeanInfo(cp.getClass());
             PropertyDescriptor[] props = targetBeanInfo.getPropertyDescriptors();
@@ -110,7 +126,7 @@ public final class ExpressUtil {
                 String propName = prop.getName();
                 if ("class".equals(propName)) continue;
                 Object value = prop.getReadMethod().invoke(cp);
-                context.set(propName, value);
+                ctx.set(propName, value);
             }
         }
     }
